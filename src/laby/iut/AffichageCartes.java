8964c1aeb94619f96eb1c5bcc169b3d8c03d6057
aurelien.signe@gmail.com <aurelien.signe@gmail.com@0805b250-adcb-11de-9d58-61445d29f5b2>
2009-11-15 19:53:58 +0000

    Modifications disposition éléments autour du plateau + correction derniers bugs concernant l'annulation d'un coup

--- src/laby/iut/AffichageCartes.java
+++ src/laby/iut/AffichageCartes.java
@@ -6,6 +6,8 @@ import Java.*;
 import android.app.Activity;
 //import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
@@ -17,40 +19,52 @@ public class AffichageCartes extends Activity {
 	Partie partie1;
     Button retour;
     
-    TextView Text01;
+    TextView nbCartesRestantes;
     
     ArrayList<Carte> listeCarte;
        
 	public void onCreate(Bundle savedInstanceState) {
     	
     	super.onCreate(savedInstanceState);
+    	setFullscreen();
         setContentView(R.layout.affichagecartesjoueur);
+        nbCartesRestantes = (TextView) findViewById(R.id.nbCartesRestantes);
         
         listeCarte = new ArrayList<Carte>();    
         
 					        //Intent getIntent=getIntent();
 					        //Bundle objet = this.getIntent().getExtras();
 					      //  listeCarte = objet.getParcelableArrayList("listeCarte");
-					        
-					        
 					        //monJoueur = objetarret.
         
         j1=new Utilisateur("j1");
         j2=new Utilisateur("j2");
-        j3=new Utilisateur("j3");
-				       		//j4=new Utilisateur("j4");
+        //j3=new Utilisateur("j3");
+		//j4=new Utilisateur("j4");
         partie1 = new Partie("partie1");
         
         j1.RejoindrePartie(partie1);	
         j2.RejoindrePartie(partie1);
-        j3.RejoindrePartie(partie1);				    
+        //j3.RejoindrePartie(partie1);				    
               
         partie1.lancerPartie();
           
         
+        
         GridView gridview = (GridView) findViewById(R.id.gridview);
-        gridview.setAdapter(new ImageAdapter(this, j1));
+        int nbCartes=j1.getListCarte().size();
+        nbCartesRestantes.setText("Nombre de cartes restantes : "+(nbCartes-1));
+        gridview.setAdapter(new ImageAdapter(this, j1, nbCartes));
+          
+         
+         
 	}
+	
+	public void setFullscreen() { 
+	    requestWindowFeature(Window.FEATURE_NO_TITLE); 
+	    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+	            WindowManager.LayoutParams.FLAG_FULLSCREEN); 
+	} 
     
     
 }
